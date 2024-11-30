020a9e6591da4e465b6b88bb4e49b7e557e2becd
Nagamallesh001 <nagamallesh2595@gmail.com>
2024-11-30 19:11:18 +0530

    new-project

---
+++ src/components/Main_Page/MainPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import CourseTypes from '../pagers/courseType/CourseTypes';
+import StudentRegistrations from '../pagers/studentRegistration/StudentRegistrations';
+import Courses from '../pagers/course/Courses';
+
+function MainPage() {
+  return (
+    <Router>
+      <div>
+        <nav>     
+               <ul>
+            <li><Link to="/course-types">Course Types</Link></li>
+            <li><Link to="/courses">Courses</Link></li>
+            <li><Link to="/registrations">Student Registrations</Link></li>
+          </ul>     
+             </nav>
+        <Routes>
+          <Route path="/course-types" element={<CourseTypes />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/registrations" element={<StudentRegistrations />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default MainPage;
